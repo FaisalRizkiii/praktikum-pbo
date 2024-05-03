f47959562a2db6522a9864cb0777bd20f6a1bb80
FaisalRizkiii <160577005+FaisalRizkiii@users.noreply.github.com>
2024-05-03 23:17:30 +0700

    Update TestSenjata.java

--- Praktikum 7/10e-latihanMedium/TestSenjata.java
+++ Praktikum 7/10e-latihanMedium/TestSenjata.java
@@ -18,7 +18,7 @@ public class TestSenjata {
     
     public static void main(String[] args){
         Senjata ak47 = new Senjata ("TAR");
-        Senjata m16 = new Senjata ("TAR");
+        Senjata m16 = new Senjata ("DOR");
         KontrolSenjata kontrolAK47 = new KontrolSenjata(ak47);
         KontrolSenjata kontrolm16 = new KontrolSenjata(m16);
         
